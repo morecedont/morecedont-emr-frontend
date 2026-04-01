16b201d21212d39a2c61f5ef8441d5310ba19c56
luigi <luisaugustomaestre@gmail.com>
2026-03-31 22:49:33 -0300

    fix: async function

--- src/app/(dashboard)/patients/[id]/page.tsx
+++ src/app/(dashboard)/patients/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function PatientPage({ params }: { params: { id: string } }) {
+export default async function PatientPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <div>
-      <h1>Paciente {params.id}</h1>
+      <h1>Paciente {id}</h1>
     </div>
-  )
+  );
 }
